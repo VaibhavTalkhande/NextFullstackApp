f41d94b5182d851e5f4901e20e887f3804b2ba1b
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-08-11 21:00:17 +0530

    then catch

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
-export async function connect() {
+export  async function connect() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!);
+    await mongoose
+      .connect(process.env.MONGODB_URI!)
+      .then(() => console.log("MongoDB connected"))
+      .catch((err) => console.log(err));
 
     const connection = mongoose.connection;
+
     connection.on("open", () => {
       console.log("MongoDB database connection established successfully");
-    });
+    }); 
     connection.on("error", (err) => {
       console.log(
         "MongoDB connection error. Please make sure MongoDB is running. " + err
