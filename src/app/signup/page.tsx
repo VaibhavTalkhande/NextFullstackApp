d9c622bed96e0ea7a74563faf9187b955333db2a
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-08-11 19:23:13 +0530

    signup page style

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,11 +5,13 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import {on } from "events";
 export default function SignupPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisabeled, setButtonDisabeled] = React.useState(false);//buttonDisabeled
   const onSignup = async () => {
     }
   return (
@@ -19,19 +21,19 @@ export default function SignupPage() {
       <label htmlFor="username">Username</label>
       <input
         type="text"
-        placeholder="username"
+        placeholder="username" className="border-2 border-gray-500 rounded-md p-2 m-2"
         onChange={(e) => setUser({ ...user, username: e.target.value })}
       />
       <label htmlFor="email">Email</label>
       <input
         type="email"
-        placeholder="email"
+        placeholder="email" className="border-2 border-gray-500 rounded-md p-2 m-2"
         onChange={(e) => setUser({ ...user, email: e.target.value })}
       />
       <label htmlFor="password">Password</label>
       <input
         type="password"
-        placeholder="password"
+        placeholder="password" className="border-2 border-gray-500 rounded-md p-2 m-2"
         onChange={(e) => setUser({ ...user, password: e.target.value })}
       />
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={onSignup}>
