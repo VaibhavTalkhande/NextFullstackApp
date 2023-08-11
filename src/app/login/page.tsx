1abbeaa4566b59be79741d7f1ff06e014b6ea368
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-08-11 11:02:49 +0530

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 export default function LoginPage() {
-    return (
-        <div className="flex">
-            <h1 className="text-2xl font-semibold">Login</h1>
-        </div>
-    )
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const onLogin = async () => {
+    }
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
+      <h1 className="text-2xl font-semibold">login</h1>
+        <hr />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        placeholder="email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={onLogin}>
+        Signup
+      </button>
+      <Link href="/signup">Register here</Link>
+    </div>
+  );
 }
