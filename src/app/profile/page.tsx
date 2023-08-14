73dddd31ff076c39bc280f19ef5c11b3d55f7677
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-08-14 20:09:40 +0530

    logou btn

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,55 @@
+"use client";
+import Link from "next/link";
+import axios from "axios";
+import toast from "react-hot-toast";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-            <h1 className="text-2xl font-semibold">Profile</h1>
-            <hr />
-            <p>Profile page</p>
-        </div>
-    );
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
+  const logout = async () => {
+    try {
+      const res = await axios.get("/api/users/logout");
+      toast.success("Logout successfull");
+      console.log(res.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.response.data);
+      toast.error(error.response.data);
+    }
+  };
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
+      <h1 className="text-2xl font-semibold">Profile</h1>
+      <hr />
+      <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <hr />
+      <button
+        className="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded"
+        onClick={logout}
+      >
+        {" "}
+        Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        GetUser Details
+      </button>
+    </div>
+  );
 }
