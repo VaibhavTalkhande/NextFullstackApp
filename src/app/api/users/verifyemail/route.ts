b45e14476f7c39eec6a52c77c6ff7b127fba727a
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-08-19 10:40:54 +0530

    typo

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,7 +1,6 @@
 import { connect } from "@/dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
-import { use } from "react";
 
 connect();
 
@@ -11,35 +10,26 @@ export async function POST(request: NextRequest) {
     const { token } = reqBody;
     console.log(token);
 
-    const user = await User.findOne({ verifyToken: token });
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpire: { $gt: Date.now() },
+    });
+
     if (!user) {
-      return NextResponse.json(
-        {
-          error: "Invalid token",
-        },
-        {
-          status: 400,
-        }
-      );
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
     }
     console.log(user);
+
     user.isVerified = true;
     user.verifyToken = undefined;
-    user.verifyTokenExpiry = undefined;
+    user.verifyTokenExpire = undefined;
     await user.save();
 
     return NextResponse.json({
       message: "Email verified successfully",
       success: true,
     });
-  } catch (err: any) {
-    return NextResponse.json(
-      {
-        error: err.message,
-      },
-      {
-        status: 500,
-      }
-    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
