2a7866e1183359a9150f34b0e62cb99da07b7c4f
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-08-19 15:57:50 +0530

    copy config from ogrepo torectifymistakeinspelling

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,24 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-export  async function connect() {
-  try {
-    await mongoose
-      .connect(process.env.MONGODB_URI!)
-      .then(() => console.log("MongoDB connected"))
-      .catch((err) => console.log(err));
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
 
-    const connection = mongoose.connection;
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
+        })
 
-    connection.on("open", () => {
-      console.log("MongoDB database connection established successfully");
-    }); 
-    connection.on("error", (err) => {
-      console.log(
-        "MongoDB connection error. Please make sure MongoDB is running. " + err
-      );
-      process.exit();
-    });
-  } catch (error) {
-    console.log(error);
-  }
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
+        })
+
+    } catch (error) {
+        console.log('Something goes wrong!');
+        console.log(error);
+        
+    }
+
+
 }
